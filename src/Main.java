8a29c2a8db63e45541fa9afcabd7aacbd31e41da
Daniil Parniukov <dparniukov@gmail.com>
2024-12-12 01:34:52 +0200

    fixed a bug that caused the panel to be empty sometimes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        MouseMemeResizePanel panel = new MouseMemeResizePanel();
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 1000);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.add(panel);
-        frame.setVisible(true);
-        frame.pack();
+        try {
+            MouseMemeResizePanel panel = new MouseMemeResizePanel("resources/meme.png");
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1000, 1000);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+            frame.add(panel);
+            frame.setVisible(true);
+        } catch (IOException e) {
+            System.out.println("Can't open the meme: " + e.getMessage());
+        }
     }
 }

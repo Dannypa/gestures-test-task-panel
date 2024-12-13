3d4dde6efaaaa03d5303171b45374fb0468898da
Daniil Parniukov <dparniukov@gmail.com>
2024-12-13 21:24:54 +0200

    extracted constants for screen size

--- src/MemeResizeFrame.java
+++ src/MemeResizeFrame.java
@@ -5,10 +5,13 @@ import java.awt.image.BufferedImage;
  * Frame that handles the MouseResize panel.
  */
 public class MemeResizeFrame extends JFrame {
+    final int SCREEN_WIDTH = 1000;
+    final int SCREEN_HEIGHT = 1000;
+
     MemeResizeFrame(BufferedImage meme) {
         MemeResizePanel panel = new MemeResizePanel(meme);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000, 1000);
+        this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.add(panel);

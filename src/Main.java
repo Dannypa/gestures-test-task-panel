b1f990514eeed14c1abdc5c145b0a4bcb9daca39
Daniil Parniukov <dparniukov@gmail.com>
2024-12-16 00:31:22 +0200

    removed unnecessary imports

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
         new MouseFollowResizeFrame();
--- src/MouseFollowResizeFrame.java
+++ src/MouseFollowResizeFrame.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 /**
  * Frame that handles the MouseResize panel.
